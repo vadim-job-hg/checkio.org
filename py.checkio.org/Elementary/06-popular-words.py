c7c1317650eab14b60a899a2deeb85b5fc0d2887
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-02-27 11:28:34 +0200

    :alien:

---
+++ py.checkio.org/Elementary/06-popular-words.py
@@ -0,0 +1,26 @@
+def popular_words(text, words):
+    # your code here
+    return {word:text.replace(","," ").replace("\n"," ").replace("."," ").lower().split().count(word) for word in words}
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One,
+I had just begun.
+When I was Two,
+I was nearly new.
+''', ['i', 'was', 'three']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
