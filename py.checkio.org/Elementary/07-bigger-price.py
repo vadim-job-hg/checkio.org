4a439a23fa3f339ac4ffe9655c78c0635e807743
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-02-27 11:50:10 +0200

    :alien:

---
+++ py.checkio.org/Elementary/07-bigger-price.py
@@ -0,0 +1,35 @@
+def bigger_price(limit, data):
+    """
+        TOP most expensive goods
+    """
+    # your code here
+    return sorted(data, key=lambda x: x['price'], reverse=True)[:limit]
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    print('Example:')
+    pprint(bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]))
+
+    # These "asserts" using for self-checking and not for auto-testing
+    assert bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]) == [
+        {"name": "wine", "price": 138},
+        {"name": "bread", "price": 100}
+    ], "First"
+
+    assert bigger_price(1, [
+        {"name": "pen", "price": 5},
+        {"name": "whiteboard", "price": 170}
+    ]) == [{"name": "whiteboard", "price": 170}], "Second"
+
+    print('Done! Looks like it is fine. Go and check it')
