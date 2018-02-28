980cef8e119ca2d633f7441c1f7ab5697d30ff69
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-02-28 12:37:46 +0200

    :alien:

---
+++ py.checkio.org/Elementary/10-even-last.py
@@ -0,0 +1,13 @@
+def checkio(array):
+    """
+        sums even-indexes elements and multiply at the last
+    """
+    return 0 if len(array)==0 else sum(array[0::2])*array[-1]
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
---
+++ py.checkio.org/Elementary/11-secret-message.py
@@ -0,0 +1,10 @@
+def find_message(text):
+    """Find a secret message"""
+    return ''.join(i for i in text if i.isupper())
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
