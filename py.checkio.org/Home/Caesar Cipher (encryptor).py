6d1415fcc85313d22a062e71ba4e174bec2063b7
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-06-19 17:57:38 +0300

    :alien:

---
+++ py.checkio.org/Home/Caesar Cipher (encryptor).py
@@ -0,0 +1,15 @@
+def to_encrypt(text, delta):
+    #replace this for solution
+    return ''.join([[chr(ord(x)+delta), x][x==' '] for x in text])
+
+if __name__ == '__main__':
+    print("Example:")
+    print(to_encrypt('abc', 10))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert to_encrypt("a b c", 3) == "d e f"
+    assert to_encrypt("a b c", -3) == "x y z"
+    assert to_encrypt("simple text", 16) == "iycfbu junj"
+    assert to_encrypt("important text", 10) == "swzybdkxd dohd"
+    assert to_encrypt("state secret", -13) == "fgngr frperg"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
