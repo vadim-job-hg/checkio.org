5136e97f953034574e0874dfc7dc41e6a4323d2b
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-07-03 15:29:02 +0300

    :alien:

---
+++ Mine/Skew-symmetric Matrix.py
@@ -0,0 +1,24 @@
+def checkio(matrix):
+    for y in range(len(matrix)):
+        for x in range(len(matrix)):
+            if(x==y and matrix[y][x]!=0):
+                return False
+            if(matrix[y][x]!=-matrix[x][y]):
+                return False
+    return True
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]) == True, "1st example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 1, 1],
+        [-2, -1, 0]]) == False, "2nd example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-3, -1, 0]]) == False, "3rd example"
